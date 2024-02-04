4f8ba9b690f7863a85622f22c821acab12c0e087
MANAVJOSHI555 <154759536+MANAVJOSHI555@users.noreply.github.com>
2024-02-04 22:56:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,7 @@ st.title('Hello 👋This is my first WebApp in Streamlit')
 
 
 
-'''
-with st.sidebar:
-  
-  st.title('*THANKS 🙏🏻 FOR ATTENDING ML BOOTCAMP*')
-  st.divider()
 
-  st.write('Dear incredible students, Heartfelt gratitude for your exceptional dedication and enthusiasm during the ML Bootcamp. Your passion and hard work have made this program a resounding success. Thank you for being part of this transformative learning experience. Wishing you continued success in your journey with machine learning.')
-  st.divider()'''
   
 st.divider()
 with st.expander('Project Features'):
@@ -68,7 +61,7 @@ def main():
     st.success(prediction)
 
     st.divider()
-    st.subheader('Made with 💌 by GDSC KIET')
+
 
 if __name__ == '__main__':
     main()
